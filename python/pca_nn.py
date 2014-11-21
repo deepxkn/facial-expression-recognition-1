fbb9bb53d542ae9815d86b9e36cb2e38a1f64db9
Tal Friedman <talf301@gmail.com>
2014-11-21 14:13:38 -0500

    Added some nn stuff
    Former-commit-id: 37a13f1afbe2bb2f23e6fc1252dbf17c497cc931

--- python/pca_nn.py
+++ python/pca_nn.py
@@ -1,16 +1,17 @@
 import os
 import sys
-from util import standardize, shuffle_in_unison, load_labeled_training
+from util import standardize, shuffle_in_unison, load_labeled_training, load_pca_proj
 from pybrain.tools import shortcuts
 from pybrain.tools import validation
 from pybrain.structure import SoftmaxLayer
 from pybrain.utilities import percentError
 from pybrain.datasets import ClassificationDataSet
-from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.supervised.trainers import BackpropTrainer, RPropMinusTrainer
 from pybrain.tools.validation import Validator
 def main():
-    images, labels = load_labeled_training(flatten=True)
-    images = standardize(images)
+    #images, labels = load_labeled_training(flatten=True)
+    #images = standardize(images)
+    images, labels = load_pca_proj(K=100)
     shuffle_in_unison(images, labels)
     ds = ClassificationDataSet(images.shape[1],1, nb_classes=7)
     for i,l in zip(images, labels):
@@ -19,8 +20,10 @@ def main():
     test, train = ds.splitWithProportion(0.2)
     test._convertToOneOfMany()
     train._convertToOneOfMany()
-    net=shortcuts.buildNetwork(1024, 100, train.outdim, outclass=SoftmaxLayer)
-    trainer = BackpropTrainer(net, dataset=train, momentum=0.1, learningrate=0.1)
+    net=shortcuts.buildNetwork(train.indim, 50,train.outdim, outclass=SoftmaxLayer)
+
+    trainer = BackpropTrainer(net, dataset=train, momentum=0.1, learningrate=0.01, weightdecay=0.05)
+    #trainer = RPropMinusTrainer(net, dataset=train)
     #cv = validation.CrossValidator(trainer, ds)
     #print cv.validate()
     net.randomize()
@@ -35,7 +38,10 @@ def main():
         trainer.trainEpochs(10)
         trnres = percentError(trainer.testOnClassData(dataset=train), train['class'])
         testres = percentError(trainer.testOnClassData(dataset=test), test['class'])
-        print "Iteration: %d, Training error: %.10f, Test error: %.10f" % (trainer.totalepochs, trnres, testres)
+        trnmse = trainer.testOnData(dataset=train)
+        testmse = trainer.testOnData(dataset=test)
+        print "Iteration: %d, Training error: %.5f, Test error: %.5f" % (trainer.totalepochs, trnres, testres)
+        print "Training MSE: %.5f, Test MSE: %.5f" % (trnmse, testmse)
     
 
 if __name__ == '__main__':
