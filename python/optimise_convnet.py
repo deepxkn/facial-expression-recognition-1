35c106ee08bdfab8f59b095375c200a2c26bc137
Erin Grant <e.grant41@gmail.com>
2014-11-22 12:59:33 -0500

    setup for spearmint hyperparameter optimisation
    Former-commit-id: fd5489bef1f23a50c89ba3b7bd9f26fb285fbf67

--- python/optimise_convnet.py
+++ python/optimise_convnet.py
@@ -1,43 +1,14 @@
+# wrapper for spearmint hyperparameter optimisation
 from convnet import *
 import util
-import itertools
 import numpy as np
-import theano.tensor as tensor
 import pprint
 
-parameters = {
-    # hyperparameters
-    'number of epochs' :                    (100,),
-    'batch size' :                          (30, 50, 100),
-    'filter size' :                         ((3, 3), (4, 4), (5, 5),),
-    'number of kernels' :                   ((20, 30, 40), (20, 50, 100)),
-    'pool size' :                           ((2, 2),),
-    'learning rate' :                       (10**x for x in range(-2, 0)),
-    'learning rate decay' :                 (None,),
-    'patience' :                            (10000,),
-    'patience increase' :                   (2,),
-    'improvement threshold' :               (0.995,),
-    # architecture
-    'convpool layer activation function':   (tensor.tanh,),
-    'hidden layer activation function':     (tensor.tanh,),
-    'number of convpool layers' :           (1, 2, 3),
-    'number of hidden layers' :             (1, 2),
-    'number of hidden units' :              (50, 100, 500, 1000)
-}
-
-experiment_conditions = [dict(zip(parameters, x)) for x in itertools.product(*parameters.values())]
-
 labeled_training, labeled_training_labels = util.load_labeled_training(flatten=True)
-labeled_training -= np.mean(labeled_training)
 
-#from zca import ZCA
-#zca = ZCA().fit(labeled_training)
-#labeled_training = zca.transform(labeled_training)
-#render_matrix(labeled_training[:100,:], flattened=True)
-
-#render_matrix(labeled_training[:100,:], flattened=True)
-#labeled_training = global_contrast_normalize(labeled_training, use_std=True)
-#render_matrix(labeled_training[:100,:], flattened=True)
+from zca import ZCA
+zca = ZCA().fit(labeled_training)
+labeled_training = zca.transform(labeled_training)
 
 # dumb validation set partition for now
 util.shuffle_in_unison(labeled_training, labeled_training_labels)
@@ -45,36 +16,24 @@ valid_split = labeled_training.shape[0] // 4
 train_data, train_labels = (labeled_training[valid_split:, :], labeled_training_labels[valid_split:])
 valid_data, valid_labels = (labeled_training[:valid_split, :], labeled_training_labels[:valid_split])
 
-f = open('out.txt', 'w')
-
-
-for i in range(len(experiment_conditions)):
-    cond = experiment_conditions[i]
-    print '==================================================================='
-    sys.stderr.write('condition number ' + str(i) + ' out of ' + str(len(experiment_conditions)) + '\n')
-    sys.stderr.write(str(cond) + '\n')
-    f.write('============================================================')
-    pprint.pprint(cond)
-    f.write(str(cond))
-    f.write('\n')
-    loss, _ = evaluate_lenet5(
-        learning_rate=cond['learning rate'],
-        n_epochs=cond['number of epochs'],
-        patience=cond['patience'],
-        patience_increase=cond['patience increase'],
-        improvement_threshold=cond['improvement threshold'],
-        nkerns=cond['number of kernels'],
-        batch_size=cond['batch size'],
-        filter_size =cond['filter size'],
-        pool_size = cond['pool size'],
-        n_convpool_layers = cond['number of convpool layers'],
-        n_hidden_layers = cond['number of hidden layers'],
-        n_hidden_units = cond['number of hidden units'],
-        convpool_layer_activation=tensor.tanh,
-        hidden_layer_activation=tensor.tanh,
+def main(job_id, params):
+    pprint.pprint(params)
+    return evaluate_lenet5(
+        learning_rate=params['learning rate'],
+        learning_rate_decay=params['learning rate decay'],
+        n_epochs=params['number of epochs'],
+        patience=params['patience'],
+        patience_increase=params['patience increase'],
+        improvement_threshold=params['improvement threshold'],
+        nkerns=[int(n) for n in params['number of kernels'].split()],
+        batch_size=params['batch size'],
+        filter_size = (params['filter size'], params['filter size'])
+        pool_size = (params['pool size'], params['pool size'])
+        n_convpool_layers = params['number of convpool layers'],
+        n_hidden_layers = params['number of hidden layers'],
+        n_hidden_units = params['number of hidden units'],
+        convpool_layer_activation=params['convpool layer activation function'],
+        hidden_layer_activation=params['hidden layer activation function'],
         training_data=(train_data, train_labels),
         validation_data=(valid_data, valid_labels)
     )
-    f.write('best validation loss: ' + str(loss))
-    sys.stderr.write('best validation loss: ' + str(loss) + '\n')
-    f.write('\n')
