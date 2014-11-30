2877c6ea19d5c3f4e489d72fde6530dec3696973
Tal Friedman <talf301@gmail.com>
2014-11-30 18:46:55 -0500

    Added stuff for making svm tables
    Former-commit-id: b8cadaf42ebe1a31fd0a2f644082bf2a8e2fbf22

--- python/pca_svm.py
+++ python/pca_svm.py
@@ -7,20 +7,24 @@ from sklearn import cross_validation
 
 def main():
    k=500
-   proj_test, labels = load_pca_proj(K=k)
-   shuffle_in_unison(proj_test, labels)
-   #images, labels = load_labeled_training(flatten=True)
+      #images, labels = load_labeled_training(flatten=True)
    #images = standardize(images)
    #shuffle_in_unison(images, labels)
-   svc = SVC(kernel='rbf')
-   scores = cross_validation.cross_val_score(svc, proj_test, labels, cv=10)
-   print scores
-   print np.mean(scores)
-   print np.var(scores)
-   pt = load_pca_test(K=k)
-   svc.fit(proj_test, labels)
-   pred = svc.predict(pt)
-   write_results(pred, '../svm_res.csv')
+   for k in [100,500,1024]:
+      proj_test, labels = load_pca_proj(K=k)
+      shuffle_in_unison(proj_test, labels)
+      for ker in ['linear', 'sigmoid', 'rbf']:
+           svc = SVC(kernel=ker)
+           scores = cross_validation.cross_val_score(svc, proj_test, labels, cv=10)
+           print "Kernel: " + ker
+           print "K: " + str(k)
+           print scores
+           print np.mean(scores)
+           print np.var(scores)
+   #pt = load_pca_test(K=k)
+   #svc.fit(proj_test, labels)
+   #pred = svc.predict(pt)
+   #write_results(pred, '../svm_res.csv')
 
 if __name__ == '__main__':
     sys.exit(main())
