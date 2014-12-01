72826e3e5a00e4b539c4a7cdf54cc380a78b2281
Erin Grant <e.grant41@gmail.com>
2014-12-01 17:00:07 -0500

    cleanup

--- python/test_convnet.py
+++ python/test_convnet.py
@@ -35,21 +35,20 @@ def results_extractor(train_obj):
     )
 
 def test_convolutional_network():
-    yaml = open("conv_large_filters.yaml", 'r').read()
+    yaml = open("conv_no_valid_with_100_components.yaml", 'r').read()
 
     hyper_params = {
                     'learning_rate': 0.2,
+                    'init_momentum': 0.5,
                     'batch_size': 40,
                     'output_channels_conv1': 20,
                     'output_channels_conv2': 50,
                     'output_channels_conv3': 80,
                     'max_epochs': 100,
                     'num_hiddens_h1' : 256,
-                    'num_hiddens_h2' : 256,
                     }
     yaml = yaml % (hyper_params)
     train = yaml_parse.load(yaml)
     train.main_loop()
 
-if __name__ == '__main__':
-    test_convolutional_network()
+if __name__ == '__main__': test_convolutional_network()
